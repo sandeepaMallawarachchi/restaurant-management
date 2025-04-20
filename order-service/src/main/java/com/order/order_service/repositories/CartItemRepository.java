478ae5fcbbd1a083c303826d2459d813950f3c54
Sachini Dilrangi <sdilrangi58@gmail.com>
2025-04-20 16:45:57 +0530

    created repository classes

---
+++ order-service/src/main/java/com/order/order_service/repositories/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.order.order_service.repositories;
+
+import com.order.order_service.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+}
---
+++ order-service/src/main/java/com/order/order_service/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.order.order_service.repositories;
+
+import com.order.order_service.models.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+}
